7a7d5b838952db8d0d51b3be8a82de0f40c478e4
stan e <stan.e@hotmail.com>
2015-11-25 08:41:44 +0100

    24-11-15

---
+++ login.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>DeServiceGroup</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div class="header">
+            <div class="wrapper">
+                <div class="sitenameblock"><a class="sitename" href="index.html"><span class="bold">Deservice</span><span class="italic">Group</span></a></div>
+                <ul class="forheader">
+                    <li><a href="aboutus.html">over ons</a></li>
+                    <li><a href="webshop.html">webshop</a></li>
+                    <li><a href="contact.html">contact</a></li>
+                </ul>
+                <ul class="rightlist">
+                    <li class="active"><a href="loggedin.html">aanmelden</a></li>
+                </ul>
+            </div>
+        </div>
+        <br/><br/><br/>
+     	<div class="login">
+     		<h3 class="login">inloggen</h3>
+     		<form class="login">
+     			e-mail <input type="text" name="mail"><br/>
+     			wachtwoord <input tpye="password" name="password">
+     		</form>
+     	</div>
+    </body>
+</html>
