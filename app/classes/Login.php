ea221f79447dfd176e01d82d96b2ae2bbdd8aa1d
mryannick <mryannickz@live.nl>
2015-11-25 09:42:12 +0100

    mappenstructuur
    Mappen structuur is aangepast, routeclass is aangemaakt

---
+++ app/classes/Login.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ *  Author: Yannick Berendsen & Arnold Buntsma
+ *  Datum: 25-11-2015
+ *  Classe: Login Class
+ *  Desc: Inloggen voor gebruikers en moderatoren
+ *
+ */
+
+class Login{
+    public $email;
+    public $password;
+    protected $attempts;
+}
