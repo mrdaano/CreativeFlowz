29bd0945dd19875c6f6af3f9d5976371b5eedc12
baruchbramer <baruchbramer@outlook.com>
2015-12-02 14:40:45 +0100

    product class aangemaakt

--- app/classes/Database.php
+++ app/classes/Database.php
@@ -6,7 +6,7 @@ class Database {
 	private $_pdo, $_query, $_results, $_count = 0, $_error = false, $_sql, $_values = array();
 	
 	public function __construct() {
-		$this->_pdo = new PDO('mysql:host=localhost;dbname=cursus', 'root', '');
+		$this->_pdo = new PDO('mysql:host=localhost:3307;dbname=mydb', 'root', 'usbw');
 	}
 	
 	/**
--- product.php
+++ product.php
@@ -1,5 +1,29 @@
 <?php
 include_once 'app/classes/Product.php';
+include_once 'app/classes/Database.php';
+
+
+//Product toevoegen
+if (isset($_GET['n'])) { ?>
+	<form action="" method="post">
+		Naam: <input type="text" name="name"><br>
+		Code: <input type="text" name="code"><br>
+		Tweede hands: <select name="secondhand">
+			<option value="0">Nee</option>
+			<option value="1">Ja</option>
+		</select><br>
+		Beschrijving: <textarea name="description"></textarea><br>
+		Leverancier: <select name="supplier">
+			PHPPPPPP
+		</select><br>
+		Prijs: <input type="text" name="price"><br>
+		PHPPPPP
+		<input type="submit">
+	</form>
+<?php
+}
+
+
 
 
 ?>
