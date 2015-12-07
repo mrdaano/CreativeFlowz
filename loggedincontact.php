fcd989537b29f758176bc2bf16fd5f9987948a02
stan e <stan.e@hotmail.com>
2015-12-07 13:04:30 +0100

    contact update

--- loggedincontact.php
+++ loggedincontact.php
@@ -37,29 +37,39 @@
             <div class="wrapper">
                 <div class="fourthheader">
                   <table class="thirdheadertable">
-                    <th class="thirdheaderhead">contact</th>
-                      <tr class="thirdheaderdata">
-                        <td class="textintable">De servicegroup</td>
-                      </tr>
-                      <tr class="thirdheaderdata">
-                        <td class="textintable">Bart de Jong</td>
-                      </tr><tr class="thirdheaderdata">
-                          <td class="textintable">De Karekiet 32</td>
-                      </tr><tr class="thirdheaderdata">
-                        <td class="textintable">7671 LA Vriezenveen</td>
-                      </tr><tr class="thirdheaderdata">
-                        <td class="textintable">The Netherlands</td>
-                      </tr><tr class="thirdheaderdata">
-                        <td class="textintable">Telefoon: +31546-564663</td>
-                      </tr><tr class="thirdheaderdata">
-                        <td class="textintable">Fax: +31546-642186</td>
-                      </tr><tr class="thirdheaderdata">
-                        <td class="textintable">Mobiel: +31653234496</td>
-                      </tr><tr class="thirdheaderdata">
-                        <td class="textintable">E-mail: moetnogkomen@idk.com</td>
-                      </tr><tr class="thirdheaderdata">
-                        <td class="textintable">K.v.k. Enschede nummer: 06083741</td>
-                      </tr>
+                    <tr class="thirdheaderhead">
+                      <td class="textintable">contact</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">De servicegroup</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">Bart de Jong</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                        <td class="textintable">De Karekiet 32</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">7671 LA Vriezenveen</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">The Netherlands</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">Telefoon: +31546-564663</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">Fax: +31546-642186</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">Mobiel: +31653234496</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">E-mail: moetnogkomen@idk.com</td>
+                    </tr>
+                    <tr class="thirdheaderdata">
+                      <td class="textintable">K.v.k. Enschede nummer: 06083741</td>
+                    </tr>
                   </table>
             </div>
         </div>
