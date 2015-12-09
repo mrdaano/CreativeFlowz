9d99ba158828d483bc0c691b2ad343ccbdafbb5b
Daan Kloetstra <daankloetstra@hotmail.com>
2015-12-09 13:00:39 +0100

    Table design

--- app/cms/modules/mediamanagment.php
+++ app/cms/modules/mediamanagment.php
@@ -1,6 +1,7 @@
 
-        <div class="neworders">
+        <div class="neworders media_beheer">
               <h1>Media beheer</h1>
+              <a href="#" class="btn">Voeg bestand toe</a>
               <table class="media_table">
 	              <thead>
 		              <tr>
@@ -9,5 +10,17 @@
 			              <th>Acties</th>
 		              </tr>
 	              </thead>
+	              <tbody>
+		              <tr>
+			              <td><a href="#">Test.png</a></td>
+			              <td>Image</td>
+			              <td><a href="#" class="btn">Verwijderen</a></td>
+		              </tr>
+		              <tr>
+			              <td>Test.png</td>
+			              <td>Image</td>
+			              <td>Edit</td>
+		              </tr>
+	              </tbody>
               </table>
         </div>
