ea3a4240aaf596f4e5f3c0f31c7b36cf407332c7
Daan Kloetstra <daankloetstra@hotmail.com>
2015-12-15 20:47:54 +0100

    Begin of delete function

--- app/cms/modules/mediamanagment.php
+++ app/cms/modules/mediamanagment.php
@@ -21,7 +21,9 @@
 				            </div>
 				            <input type="submit" value="Uploaden" class="btn" name="submit">
 			            </form>
-		        <?php    }
+		        <?php    } else if($_GET['actie'] == "delete") {
+			        // Show delete message
+		        }
 	            } else {
 		           if (isset($_GET['success'])) {
 			           echo "<p>Bestand is geupload.</p>";
@@ -42,7 +44,7 @@
 		              <tr>
 			              <td><a href="<?php echo $item->path . '/' . $item->name; ?>" target="_blank"><?php echo $item->name; ?></a></td>
 			              <td><?php echo $media->getType($item->name); ?></td>
-			              <td><a href="#" class="btn">Verwijderen</a></td>
+			              <td><a href="index.php?page=cms&module=mediamanagment&actie=delete&id=<?php echo $item->id; ?>" class="btn">Verwijderen</a></td>
 		              </tr>
 		              <?php } ?>
 	              </tbody>
