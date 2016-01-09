a437bfbb04ef866b3fc1c56bea879c2247f9ae2f
stan e <stan.e@hotmail.com>
2016-01-09 11:13:39 +0100

    09-01-2016 11:15

--- app/pages/module/orders.php
+++ app/pages/module/orders.php
@@ -1,17 +1,30 @@
-<div class="neworders page accountsettings">
+	
 <div class="orders">
-	dsafdsafdsa
-<?php
+<div class="order">
+	<?php
 
-$myOrder = myOrder::getOrder();
+	$myOrder = myOrder::getOrder();
 
-foreach($myOrder as $key => $order) {
-	$orderline = myOrder::getOrder_line($order->id);
+		foreach($myOrder as $key => $order) {
+			$orderline = myOrder::getOrder_line($order->id);
 
-	foreach($orderline as $key => $line) {
-		$products = myOrder::getProduct($line->product_id)
+			foreach($orderline as $key => $line) {
+				$products = myOrder::getProduct($line->product_id);
+			}
+		}
 
+	?>
+	<div class="leftside">
+		order: <?php echo "{$order->id}"?><br><br>
+		products: <?php echo "{$product->name}"?><br>
+		price: <?php echo "&euro;{$price}" ?><br>
+		amount:<br>
 
-?>
+	</div>
+	<div class="rightside">
+		<br>
+		created_at:<br><br><br>
+		totalprice:
+	</div>
 </div>
 </div>
