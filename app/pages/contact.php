90bfa31312a1ef1035552b391fe4c5f627e85cf3
Arnold Buntsma <arnold8520@gmail.com>
2016-01-12 20:56:19 +0100

    contact werkt nog niet helemaal
    stuurt sws een mail ongeacht of je alles hebt ingevuld of niet

--- app/pages/contact.php
+++ app/pages/contact.php
@@ -1,7 +1,51 @@
-<?php if (isset($_POST['name']) && isset ($_POST['mail']) && isset ($_POST['subject']) && isset ($_POST['message']) && isset($_POST['g-recaptcha-response'])) {
-    header("location: http://{$_SERVER['HTTP_HOST']}/app/pages/module/mailcontact.php");
-}else
- ?>
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $messageErr = $subjectErr = $captchaErr = "";
+$name = $email = $gender = $message = $subject = $tel = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "Uw naam is vereist";
+   } else {
+     $name = $_POST["name"];
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+       $nameErr = "Uw naam mag alleen uit letters en spaties bestaan";
+     }
+   }
+
+   if (empty($_POST["mail"])) {
+     $emailErr = "Uw e-mail is vereist";
+   } else {
+     $email = $_POST["mail"];
+     // check if e-mail address is well-formed
+     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $emailErr = "Vul een correct e-mail adres in";
+     }
+   }
+
+   if (empty($_POST["subject"])) {
+     $subjectErr = "Een onderwerp is vereist";
+   } else {
+     $subject = $_POST["subject"];
+   }
+
+   if (empty($_POST["message"])) {
+     $messageErr = "Een berichicht is vereist";
+   } else {
+     $message = $_POST["message"];
+   }
+   if (empty($_POST['phone'])){
+     $tel= "";
+   }else {
+     $tel = $_POST['phone'];
+   }
+   if (empty($_POST['g-recaptcha-response'])){
+     $captchaErr = "Captcha is vereist";
+   }
+}
+?>
+
 <div class="thirdheader">
     <div class="wrapper">
         <div class="fourthheader">
@@ -27,7 +71,7 @@
               </tr><tr class="thirdheaderdata">
                 <td class="textintable">Mobiel: +31653234496</td>
               </tr><tr class="thirdheaderdata">
-                <td class="textintable">E-mail: moetnogkomen@idk.com</td>
+                <td class="textintable">E-mail: info@theservicegroup.nl</td>
               </tr><tr class="thirdheaderdata">
                 <td class="textintable">K.v.k. Enschede nummer: 06083741</td>
               </tr>
@@ -36,15 +80,21 @@
 </div>
 <div class="wrapper">
     <div class="textheader">
-      <p class="contact"> Wilt u contact met ons opnemen, kan dat ook met onderstaand formulier of de gegevens aan de linkerkant</p>
-      <br/>
-      <form method="post" class="contact">
-        Naam<br/><input type="text" name="name" class="login"><br/>
-        E-mail<br/><input type="text" name="mail" class="login"><br/>
-        Telefoon<br/><input type="text" name="phone" class="login"><br/>
-        Onderwerp<br/><input type="text" name="subject" class="login"><br/>
-        Bericht<br/><textarea name="message" rows="10" cols="40" class="contact"></textarea><br/>
+      <p class="contact"> Wilt u contact met ons opnemen, dat kan ook met onderstaand formulier of de gegevens aan de linkerkant</p>
+      <br/><br/>
+      <p><span class="error">* vereist veld.</span></p>
+      <form method="post" class="contact" action="app/pages/module/mailcontact.php">
+        Naam<br/><input type="text" name="name" class="login" value="<?php echo $name;?>">
+        <span class="error">* <?php echo $nameErr;?></span><br/>
+        E-mail<br/><input type="text" name="mail" class="login" value="<?php echo $email;?>">
+        <span class="error">* <?php echo $emailErr;?></span><br/>
+        Telefoon<br/><input type="text" name="phone" class="login" value="<?php echo $tel;?>"><br/>
+        Onderwerp<br/><input type="text" name="subject" class="login" value="<?php echo $subject;?>">
+        <span class="error">* <?php echo $subjectErr;?></span><br/>
+        Bericht<br/><textarea name="message" rows="10" cols="40" class="contact" value="<?php echo $message;?>"></textarea>
+        <span class="error">* <?php echo $messageErr;?></span><br/><br/>
         <div class="g-recaptcha" data-sitekey="6LdUCxUTAAAAAJDMYzFoW3eBP9avxZP2MO0W-ZLj"></div>
+        <span class="error">* <?php echo $captchaErr;?></span><br/>
         <input type="submit" name="send" value="verzenden" class="loginBtn">
       </form>
     </div>
--- app/pages/module/mailcontact.php
+++ app/pages/module/mailcontact.php
@@ -3,26 +3,42 @@
 Author: Arnold Buntsma
 09-12-2015
 */
+
 $name = $_POST['name'];
 $mail = $_POST['mail'];
 $tel = $_POST['phone'];
-$captcha = $_POST['g-recaptcha-response'];
 $subject = "Contact via website over: ".$_POST['subject'];
 $message = $_POST['message'];
-$to = 'theservicegroup@solcom.nl';
+$to = 'info@theservicegroup.nl';
 
-$body = "$name\n$mail\n$tel\n\n$message";
+$captcha_response = $_POST['g-recaptcha-response'];
+$secret = "6LdUCxUTAAAAAMoMNjH9ldbG8I3PvSQehxSPThvU";
+$user_ip = $_SERVER['REMOTE_ADDR'];
 
-
-if ($_POST['send']){
-  if (isset ($_POST['name']) && isset ($_POST['mail']) && isset ($_POST['subject']) && isset ($_POST['message'])){
-    if (mail ($to, $subject, $body)) {
-    echo '<p>Uw bericht is verzonden.</p><a href="http://localhost/creativeflowz/index.php?page=contact">Terug naar de site</a>';
-  } else {
-    echo '<p>Something went wrong</p>';
-    }
-  }else {
-    echo '<p>vul alle gegeves in </p>';
-  }
+if (empty($_POST['name'])
+    || empty($_POST['mail'])
+    || empty($_POST['subject'])
+    || empty($_POST['message'])
+    || empty($_POST['g-recaptcha-response'])
+    ){
+header("Location: http://{$_SERVER['HTTP_HOST']}{$_SERVER['SCRIPT_NAME']}?page=contact");
 }
+
+$mail_message = "$name\n$mail\n$tel\n\n$message";
+
+$captcha_verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$captcha_response."&remoteip=".$_SERVER['REMOTE_ADDR']);
+if ($captcha_verify.success==false){
+  die();
+}else{
+
+  if ($_POST['send']){
+    if (mail ($to, $subject, $mail_message)) {
+    echo '<p>Uw bericht is verzonden.</p><a href="http://localhost/creativeflowz/index.php?page=contact">Terug naar de site</a>';
+    } else {
+      echo '<p>Something went wrong</p>';
+      }
+    }else {
+      echo '<p>vul alle gegeves in </p>';
+    }
+  }
 ?>
